1dfd7b840e87aff7565076589fd36b1d99856773
mahabaleshwara Bairi <mahabaleshwarabairi@gmail.com>
2020-08-16 14:53:47 +0530

    adding first file

---
+++ maha/4.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 21 14:38:16 2020
+
+@author: Mahabaleshwara.Bairi
+"""
+
+age=input ("Enter Age : ")
+if int(age) >72:
+    print ("Senior Citizen")
+else:
+     print ("Not Eligible for Senior Citizen")
---
+++ maha/5.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 21 14:44:15 2020
+
+@author: Mahabaleshwara.Bairi
+"""
+
+age=input ("Enter Age : ")
+if int(age) >72:
+    print ("Senior Citizen")
+elif int (age) <20:
+     print (" Eligible for student")
+else:
+     print ("Normal Charges")
+     
